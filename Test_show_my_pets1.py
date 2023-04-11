b93c4f14e277bfc1c9f1188420da59c558134fb7
AnnaAndreeva22 <113442695+AnnaAndreeva22@users.noreply.github.com>
2023-04-11 10:24:53 +0300

    Update Test_show_my_pets1.py

--- Test_show_my_pets1.py
+++ Test_show_my_pets1.py
@@ -25,10 +25,10 @@ def testing():
 
 def test_show_my_pets():
     # Вводим email
-    pytest.driver.find_element(By.ID, 'email').send_keys('anna.an2022@inbox.ru')
+    pytest.driver.find_element(By.ID, 'email').send_keys('***')
     
     # Вводим пароль
-    pytest.driver.find_element(By.ID, 'pass').send_keys('petfriend22')
+    pytest.driver.find_element(By.ID, 'pass').send_keys('***')
 
     # Нажимаем на кнопку входа в аккаунт
     pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
